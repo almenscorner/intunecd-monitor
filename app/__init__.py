9e9449e3e4dec1e85a0a182a69a81d2b9296cda3
Tobias Almén <78877636+almenscorner@users.noreply.github.com>
2023-09-16 12:52:03 +0200

    add celery

--- app/__init__.py
+++ app/__init__.py
@@ -4,18 +4,49 @@ from flask_session import Session
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_paranoid import Paranoid
-from flask_sock import Sock
+from flask_migrate import Migrate
+from flask_apispec import FlaskApiSpec
+from werkzeug.middleware.proxy_fix import ProxyFix
+from celery import Celery, Task
 
 app = Flask(__name__)
 app.config.from_object(app_config)
-Session(app)
-db = SQLAlchemy(app)
-bcrypt = Bcrypt(app)
-sock = Sock(app)
-paranoid = Paranoid(app)
-paranoid.redirect_view = '/login'
+app.config.from_mapping(
+    CELERY=dict(
+        broker_url="redis://redis:6379/0",
+        result_backend="redis://redis:6379/0",
+        task_ignore_result=True,
+        task_track_started=True,
+    ),
+)
+app.config["APISPEC_SWAGGER_UI_URL"] = "/apidocs"
+app.config["APISPEC_TITLE"] = "IntuneCD Monitor API Docs"
 
-from werkzeug.middleware.proxy_fix import ProxyFix
-app.wsgi_app = ProxyFix(app.wsgi_app)
+with app.app_context():
+    Session(app)
+    db = SQLAlchemy(app)
+    migrate = Migrate(app, db)
+    bcrypt = Bcrypt(app)
+    paranoid = Paranoid(app)
+    paranoid.redirect_view = "/login"
+    app.wsgi_app = ProxyFix(app.wsgi_app)
+    docs = FlaskApiSpec(app)
 
-from app import routes
+    def celery_init_app(app: Flask) -> Celery:
+        class FlaskTask(Task):
+            def __call__(self, *args: object, **kwargs: object) -> object:
+                with app.app_context():
+                    return self.run(*args, **kwargs)
+
+        celery_app = Celery(app.name, task_cls=FlaskTask)
+        celery_app.config_from_object(app.config["CELERY"])
+        celery_app.set_default()
+        app.extensions["celery-worker"] = celery_app
+
+        return celery_app
+
+    celery = celery_init_app(app)
+
+    celery.conf.update({"beat_dburi": app_config.BEAT_DB_URI})
+
+    from app import routes
